ba73aa89075ee8f87ff82997bcb47d6071ce9936
Manuel Barkhau <mbarkhau@gmail.com>
2018-12-05 09:21:13 +0100

    fix bad example config

--- bootstrapit_example.sh
+++ bootstrapit_example.sh
@@ -34,7 +34,7 @@ PACKAGE_NAME="mypackagename"
 GIT_REPO_NAMESPACE="vandelay"
 GIT_REPO_DOMAIN="gitlab.com"
 
-# PACKAGE_VERSION="201908.0001-alpha"
+# PACKAGE_VERSION="v201908.0001-alpha"
 PACKAGE_VERSION="$(date + 'v%Y%m.0001-alpha')"
 
 # These must be valid (space separated) conda package names.
